bb9479f41b831a4db71f1183a0ad56b358af5ccd
J. Damian Segrelles Quilis <dquilis@dsic.upv.es>
2020-10-07 10:38:06 +0200

    Version 0

---
+++ exercise1/exercise1.c
@@ -0,0 +1,9 @@
+/******************* Problem definition **********************************/
+/* Implement a program that it shows the message "Hello World" on screen. */
+/*************************************************************************/
+
+/* Here, you must include the required libraries */
+
+void main(){
+	/* Here, you must write the source code to show the missatge "Hello world" on screen*/
+}
---
+++ exercise2/exercise2.c
@@ -0,0 +1,5 @@
+/* Here, you should entry the required libraries */
+
+void main(){
+	/* Here, you should write the source code of your program */
+}
