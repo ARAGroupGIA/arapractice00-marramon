b93ff9a7868bb423577cc9e9451d8dde33e431ba
marramon <61043057+marramon@users.noreply.github.com>
2020-10-07 16:04:23 +0200

    Update exercise2.c

--- exercise2/exercise2.c
+++ exercise2/exercise2.c
@@ -1,5 +1,16 @@
-/* Here, you should entry the required libraries */
+#include <stdio.h>
 
-void main(){
-	/* Here, you should write the source code of your program */
+#define PI 3.14159
+
+int main(){
+	float radio, area, perimeter;
+	
+	printf("Please, entry the radio: ");
+	scanf("%f", &radio);
+	
+	area      = PI*radio*radio;
+	perimeter = 2*PI*radio;
+	
+	printf("\nThe area is %f and the perimeter is %f\n", area, perimeter);
+	return 0;			
 }
